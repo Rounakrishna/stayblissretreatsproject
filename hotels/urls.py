1ccd0705f31e0d82ab5f6f52a0e5405b5cd0f3b8
Rounakrishna <krishnakumarqaz0@gmail.com>
2024-10-09 01:38:34 +0530

    hotels ad

--- hotels/urls.py
+++ hotels/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
         path('littleheartresort/',views.LITTLE_HEART_RESORT,name='littleheartresort'),
+        path('ayodhyastayhotel/',views.AYODHAYA_STAY_HOTEL,name='ayodhyastayhotel'),
+        path('hotelclassichills/',views.HOTEL_CLASSIC_CHILLS,name='hotelclassichills'),
+        path('stayblissvilla/',views.STAY_BLISS_VILLA,name='stayblissvilla'),
+        path('daffodilcottage/',views.DAFFODIL_COTTAGE,name='daffodilcottage'),
     
 ]
 
