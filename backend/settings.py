b041ddc93e7a914076689b859048f7bf042ae632
Rounakrishna <krishnakumarqaz0@gmail.com>
2024-10-13 16:15:55 +0530

    krrrf

--- backend/settings.py
+++ backend/settings.py
@@ -102,6 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://stayblissretreats.com',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
@@ -119,15 +123,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = '/var/www/stayblissretreatsproject/static/'
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'staticfile')
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  os.path.join(BASE_DIR,'media')
+
 
 
 # Default primary key field type
